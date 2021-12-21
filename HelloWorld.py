9ed5bee293f3dced0665a20d4b3d2282e674a6ca
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2021-12-21 23:05:07 +0300

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+print('_______________________________')
+print('         HELLO APP')
+print('_______________________________')
+print()
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+print(greeting)
