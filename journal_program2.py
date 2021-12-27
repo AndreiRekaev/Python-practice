4233f93bb2a3e8640dbc6915f00b4dfdd142137c
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2021-12-27 23:01:28 +0300

    Add files via upload

---
+++ journal_program2.py
@@ -0,0 +1,8 @@
+print('1. About to import journal_main')
+import journal_main
+
+print('2. Journal_main imported')
+print('3. Printing header!')
+
+journal_main.print_header()
+print('3. Done with journal_main')
