90cf80ee63f493f848491099eb5c8cb487f71df3
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2021-12-28 21:33:46 +0300

    Delete journal_program2.py

--- journal_program2.py
+++
@@ -1,8 +0,0 @@
-print('1. About to import journal_main')
-import journal_main
-
-print('2. Journal_main imported')
-print('3. Printing header!')
-
-journal_main.print_header()
-print('3. Done with journal_main')
