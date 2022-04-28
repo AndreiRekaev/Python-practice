fe405a8b33d3fdcaff4c1a5f5003a209d37580e6
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2022-04-28 09:36:49 +0300

    Update bite11.py

--- 100daysofcode/16-18 list comp and generators/bite11.py
+++ 100daysofcode/16-18 list comp and generators/bite11.py
@@ -20,49 +20,53 @@ from collections import namedtuple, defaultdict
 
 Import = namedtuple("Import", ["module", "name", "alias"])
 
+
 def main():
-  files = gen_files("../*/*.py")
-  for file_gen in files:
+    # call the generators, passing one to the other
+    files = gen_files('../*/*.py')
+    for file_gen in files:
         imports = gen_lines(file_gen)
-        
-        
+        imports = get_result(imports)
+        print_import_results(imports)
+
+
 def gen_files(pat):
     yield glob.glob(pat)
-    
-    
+
+
 def gen_lines(files):
     for file in files:
-      with open(file) as fh:
-        
-        root = ast.parse(fh.read(), file)
-        
-      for node in ast.iter_child_nodes(root):
-        if isinstance(node, ast.Import):
-          module = []
-        elif isinstance(node, ast.ImportFrom):
-          module = node.module.split('.')
-        else:
-          continue
-          
-        
-        for n in node.names:
-          yield Import(module, n.name.split('.'), n.asname)
-          
+        with open(file) as fh:
+            # https://stackoverflow.com/questions/9008451/python-easy-way-to-read-all-import-statements-from-py-module
+            root = ast.parse(fh.read(), file)
+
+        for node in ast.iter_child_nodes(root):
+            if isinstance(node, ast.Import):
+                module = []
+            elif isinstance(node, ast.ImportFrom):
+                module = node.module.split('.')
+            else:
+                continue
+
+            for n in node.names:
+                yield Import(module, n.name.split('.'), n.asname)
+
+
 def get_result(lines):
-  
-      d = defaultdict(lambda: 0)
-      for line in lines:
-         d[line.name[0]] += 1
-      return d
+    # TODO : How to convert to a dict comprehension
+    d = defaultdict(lambda: 0)
+    for line in lines:
+        d[line.name[0]] += 1
+    return d
+
 
 def print_import_results(results):
-  
-      sorted_results = sorted(results.items(), key=lambda kv: kv[1], reverse=True)
-      for desc, cnt in sorted_results:
+    sorted_results = sorted(results.items(), key=lambda kv: kv[1], reverse=True)
+    for desc, cnt in sorted_results:
         msg = f'{cnt} {desc}'
         print(msg)
-        
-        
+
+
 if __name__ == "__main__":
     main()
 
