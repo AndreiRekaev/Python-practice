e2be8c713f59cfd36d8ab95a95793ebdbda2858c
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2024-11-27 21:08:04 +0300

    Add files via upload

---
+++ coursera_python/OOP and design patterns/adapter.py
@@ -0,0 +1,18 @@
+class MappingAdapter:
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
+
+    def lighten(self, grid):
+        dim = (len(grid[0]), len(grid))
+        self.adaptee.set_dim(dim)
+        lights = []
+        obstacles = []
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                if (grid[j][i] == 1):
+                    lights.append((i, j))
+                elif (grid[j][i] == -1):
+                    obstacles.append((i, j))
+        self.adaptee.set_lights(lights)
+        self.adaptee.set_obstacles(obstacles)
+        return self.adaptee.generate_lights()
